bd94aaa5616c1fa28b44e822d148b5b5060f82e4
Дима Кучун <demon29052008@gmail.com>
2024-12-05 21:16:02 +1000

    Изучил работу с диспетчером url, передачей аргументов в url и тд.

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
-from .views import homepageview
+from django.urls import path, re_path
+from blog import views
 
-urlpatterns = [path('', homepageview, name='home')]
+urlpatterns = [
+    path('', views.index),
+    path('about/', views.about, kwargs={'name': 'Tom', 'age': 38}),
+    re_path(r'^contacts/', views.contacts),
+    re_path(r'^user/(?P<name>\D+)/(?P<age>\d+)/', views.user),
+    re_path(r'^user/(?P<name>\D+)/', views.user),
+    re_path(r'^user/', views.user),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def homepageview(request):
-    return HttpResponse('Hello, World!')
+def index(request):
+    return HttpResponse('<h2>Главная</h2>')
 
-# Create your views here.
+
+def about(request, name, age):
+    return HttpResponse(f'''
+        <h2>О пользователе</h2>
+        <p>Имя: {name}</p>
+        <p>Возраст: {age}</p>
+    ''')
+
+
+def contacts(request):
+    return HttpResponse('<h2>Контакты</h2>')
+
+
+def user(request, name='undefined', age=0):
+    return HttpResponse(f'''
+    <h2>Имя: {name}</h2>
+    <h2>Возраст: {age}</h2>
+''')
+
